c3da751f3e9c7b3b7095f37c9e7a52adc8949dbf
CsillaCsejtei <csillacsejtei@gmail.com>
2025-03-02 22:46:40 +0000

    Added footer

--- src/App.js
+++ src/App.js
@@ -6,8 +6,31 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello World</h1>
-        <Weather city="Budapest"/>
+        <Weather city="Budapest" />
       </header>
+      <footer>
+        <p>
+          This project was coded by
+          <a
+            href="https://github.com/CsillaCsejtei"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Csilla Csejtei
+          </a>
+          , is
+          <a
+            href="https://github.com/CsillaCsejtei/weather-react"
+            target="_blank" rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+          and
+          <a href="https://weather-react-hun.netlify.app/" target="_blank" rel="noopener noreferrer">
+            hosted on Netlify
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
