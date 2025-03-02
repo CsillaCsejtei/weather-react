7f0b0fa0ac885b9ae77b1caf1c4a67b53536d18e
CsillaCsejtei <csillacsejtei@gmail.com>
2025-03-02 21:43:09 +0000

    updated react version and delete react loader spinner

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,4 @@
-import React from "react";
 import axios from "axios";
-import { Circles } from "react-loader-spinner";
 
 export default function Weather(props) {
   function handleResponse(response) {
@@ -12,15 +10,5 @@ export default function Weather(props) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(handleResponse);
 
-  return (
-    <Circles
-  height="80"
-  width="80"
-  color="yellow"
-  ariaLabel="circles-loading"
-  wrapperStyle={{}}
-  wrapperClass=""
-  visible={true}
-  />
-  );
+  return "...";
 }
